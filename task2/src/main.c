e12eb906b5180d43f595b52e9a93701f650db557
TheNorth322 <lol_danchik@bk.ru>
2024-07-07 05:18:05 +0700

    add task2 solution

--- task2/src/main.c
+++ task2/src/main.c
@@ -2,18 +2,53 @@
 #include <unistd.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <stdlib.h>
 
-void fork_two_processes();
+void fork_processes();
 
-void fork_one_process();
+void print_process_info(int process_index);
 
 int main(void) {
+  fork_processes();
   return 0;
 }
 
-void fork_two_processes() {
-          
+void fork_processes() {
+  int status;
+  // process 1
+  if (fork() == 0) {
+    print_process_info(1);
+    // process 3
+    if (fork() == 0) {
+      print_process_info(3);
+      exit(3);
+    }
+    // process 4
+    if (fork() == 0) {
+      print_process_info(4);
+      exit(4);
+    }
+
+    while (wait(&status) > 0);
+    exit(1);
+  }
+  
+  // process 2
+  if (fork() == 0) {
+    print_process_info(2);
+    // process 5
+    if (fork() == 0) {
+      print_process_info(5);
+      exit(5);
+    }
+
+    while (wait(&status) > 0);
+    exit(2);
+  }
+
+  while (wait(&status) > 0);      
 }
 
-void fork_one_process();
-
+void print_process_info(int process_index) {
+  printf("Process %d\npid: %d\nppid: %d\n", process_index, getpid(), getppid());
+}
