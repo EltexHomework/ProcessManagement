e972e516bb5e9e7eb6bb45db2c0fbe8e0a987cc9
TheNorth322 <lol_danchik@bk.ru>
2024-07-07 04:42:55 +0700

    update task1 solution

--- task1/src/main.c
+++ task1/src/main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <sys//types.h>
+#include <sys/wait.h>
 
 int main(void) {
   pid_t pid = fork();
@@ -9,13 +11,16 @@ int main(void) {
   }
   // Child
   else if (pid == 0) {
-    pid_t child_pid = getpid();
-    printf("Child pid: %d\n", child_pid);
+    printf("___Child process___\nParent pid: %d\nProcess pid: %d\n", getppid(), getpid());
   } 
   // Parent
   else {
-    pid_t parent_pid = getpid();
-    printf("Parent pid: %d\n", parent_pid);
+    pid_t wait_pid;
+    int status;
+    
+    // Wait for child termination. Parent process can terminate faster than child
+    while ((wait_pid = wait(&status)) > 0);
+    printf("___Parent process___\nParent pid: %d\nProcess pid: %d\n", getppid(), getpid());
   }
   return 0;
 }
---
+++ task2/src/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+void fork_two_processes();
+
+void fork_one_process();
+
+int main(void) {
+  return 0;
+}
+
+void fork_two_processes() {
+          
+}
+
+void fork_one_process();
+
